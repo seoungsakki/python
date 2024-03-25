7cded0a8aa5660cc7fe99d058985ba75c1a4bf02
seoungsakki <164852528+seoungsakki@users.noreply.github.com>
2024-03-25 17:28:02 +0900

    3강 자료형
    python

---
+++ 3강 자료형.py
@@ -0,0 +1,115 @@
+from cospaces import *
+import math
+import random
+
+
+# a = 6
+# print("a=6\n","a>0 a<0",a>0 and a<0)
+
+
+# print("a=6")
+# print("a%2 == 0 : ",a%2==0)
+# print("a%3 == 0 : ",a%3==0)
+# print("a%4 == 0 : ",a%4==0)
+
+
+# print("a%2==0 and a%3==0 : ",a%2==0 and a%3==0)
+# print("a%3==0 and a%4==0 : ",a%3==0 and a%4==0)
+# print("a%4==0 and a%5==0 : ",a%4==0 and a%5==0)
+
+# print("a%2==0 or a%3==0 : ",a%2==0 or a%3==0)
+# print("a%3==0 or a%4==0 : ",a%3==0 or a%4==0)
+# print("a%4==0 or a%5==0 : ",a%4==0 or a%5==0)
+
+a = 6
+# print("not(a%2 == 0) : ",not(a%2==0))
+
+# print(-2)
+# print(abs(-2))
+# print("-2의 절댓값 : ",abs(-2))
+# print("2.304의 올림 : ",math.ceil(2.304))
+# print("2.304의 내림 : ",math.floor(2.304))
+# print("2.304의 반올림 : ",round(2.304))
+# print("!30 :",math.factorial(30))
+
+
+# print(3+3)
+# print(float(3+3))
+# print(int(1.1+4.5))
+
+# print("1-10까지 랜덤으로 정수 만들기 : ",random.randint(1,10))
+# print("0-1000까지 랜덤으로 정수 만들기 : ",random.randint(0,1000))
+
+
+# print(random.randrange(0,100,10))
+# print("#################################################")
+
+
+# print("라디안을 각도로 변환하면 1라디안은?",math.degrees(1))
+# print("각도를 라디안으로 변환하면? 90도는?",math.radians(90))
+
+
+# names =[롤로노아 김제민]
+
+
+# 리스트 선언 [] 안에 쓰기
+# text = "안녕하세요"
+
+# print(text)
+# print("첫 글자만 출력",text[0])
+# print("3,4번째 글자만 출력",text[2:4])
+# # 특정  글자를 출력할 때 지정하는 범위의 값은 : 시작 인덱스 번호 : 끝 인덱스 번호 +1
+# # 하 의 인덱스번호 : 2
+# # 세 의 인덱스번호 : 3
+# print("1-4번째 글자만 출력",text[3:4])
+
+# 안녕하세요
+# 0 1 2 3 4
+
+
+
+
+# text1 = """안녕하세요 저는 경기도 시흥에 살고있습니다.
+# 그리고 저는 오늘 방학식을 했습니다.
+# 개학이 오지 안았습니다."""
+
+# print(text1)
+
+# print("===========================")
+# print("안녕\n난ㄴ\n홍길동")
+# print("한 칸씩\t탭\t가")
+# print("문자열 문자열"+"더하기")
+# print(("문자열"+"곱하기")*4)
+
+
+# print("저의 이름은 %s이고 제 나이는%d입니다." % ("홍길동",20))
+
+# print(bool(0))
+# print(bool("")
+
+# print(6%2==0 and 8%2==0)
+
+
+# 각각의 변수의 값을 출력해보고,
+# 문이 열려있고 키가있음
+# 문이 닫혀있고 키가있음
+# 문이 닫혀있고 키가없음
+# 문이 열려있고 키가없음
+# 을 모두 출력하고 출력값을 확인하셈
+
+isDoorOpen=False
+isUserHasKey=True
+print(isDoorOpen and isUserHasKey)
+print(not(isDoorOpen) and isUserHasKey)
+print(not(isDoorOpen) and not(isUserHasKey))
+print(isDoorOpen and not(isUserHasKey))
+
+
+
+
+
+
+
+
+
+
