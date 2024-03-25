dc4063c99813f7960687970e5388bd0ae818c85f
seoungsakki <164852528+seoungsakki@users.noreply.github.com>
2024-03-25 17:27:35 +0900

    2강 연산하기
    python

---
+++ 2강 연산하기.py
@@ -0,0 +1,62 @@
+from cospaces import *
+import math
+import random
+
+
+# num1 = 12345678509231890284012384920841289048129481290849081290481902348021804981238490
+# num2 = 101010101010101010101010101010101010101010101101010291380128490128908902480140
+
+# print("num1= ",num1)
+# print("num2= ",num2)
+# print(num1+num2)
+# print("num1 * num2 =",num1+num2)
+
+
+# num3 = 10000
+# num4 = 1000
+# print("num3= ",num3)
+# print("num4= ",num4)
+# print(num3/num4)
+# print("num3/num4 =",num3/num4)
+
+# tall = 170
+
+# print("tall =",tall)
+# print("tall+2 =",tall + 2)
+# print("tall*2 =",tall*2) 
+# print("tall/2 =",tall//2)
+# print("나머지 =",tall%2)
+
+
+# print(num1<num2)
+# print(num2<num1)
+# print(num2<num3)
+# print(num4<num2)
+# print(num2<num4)
+# print(num1<num4)
+
+num1 = 10
+num2 = 6
+
+print(num1+num2)
+num2 = 1
+print(num1+num2)
+
+num1 = 15
+num2 = 5
+print(num1-num2)
+num2 = 10
+print(num1-num2)
+
+num1 = 5
+num2 = 2
+print(num1*num2)
+num2 = 1
+print(num1*num2)
+num1 = 10
+num2 = 1
+print(num1//num2)
+num2 = 2
+print(num1//num2)
+
+
