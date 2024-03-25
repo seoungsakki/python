7f45dd40bb0eb90d05680e38cb383bf44b523c8b
seoungsakki <164852528+seoungsakki@users.noreply.github.com>
2024-03-25 17:24:03 +0900

    1강 출력하기
    python

---
+++ 1강 출력하기.py
@@ -0,0 +1,13 @@
+from cospaces import *
+import math
+import random
+
+
+name = "오시환"
+age = 14
+school = "조남중"      #내 학교
+hobby = "게임"
+
+print("제 학교는 ",school,"입니다\n" "제 나이는 ",age,"입니다\n" "제 학교는 ",school,"입니다\n" "제 취미는 ",hobby,"입니다")
+
+
